afb62d674e00cac9cf729a96ed1816c9e0f14dc2
Jorge Mina <minajorge@hotmail.com>
2020-11-24 09:59:46 -0500

    fix config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 /** Ionic */
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -9,6 +13,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 /** Modulos */
 import { AppRoutingModule } from './app-routing.module';
 import { PrimengModule } from "src/app/shared/primeng.module";
+/** Servicios */
+import { authInterceptorProviders } from "src/app/helpers/auth.interceptor";
 /** Componentes */
 import { AppComponent } from './app.component';
 
@@ -20,6 +26,7 @@ import { AppComponent } from './app.component';
     IonicModule.forRoot(),
     AppRoutingModule,
     PrimengModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     StatusBar,
